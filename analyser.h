b6a6dc3f6ec02c59f07fc40303e55782e20a3ad6
noel <noel@noel-pc.(none)>
2013-12-20 12:26:15 +0100

    Adding Arguments parser

---
+++ analyser.h
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <errno.h> 
+#include <string.h> 
+#include <netdb.h> 
+#include <sys/types.h> 
+#include <netinet/in.h> 
+#include <sys/socket.h> 
+#include <unistd.h>
+#include <pcap.h>
+#include <net/ethernet.h>
+#include <arpa/inet.h>
+
+void parseArgs(int argc, char *argv[], char** interface, char** fichier, char** filtre, int* verbosite);
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
